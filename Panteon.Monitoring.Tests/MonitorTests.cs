9ecdacc67d209180aff173612cdd970912c7cd3c
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-03-07 10:12:21 +0200

    init

---
+++ Panteon.Monitoring.Tests/MonitorTests.cs
@@ -0,0 +1,14 @@
+﻿using Common.Testing.NUnit;
+
+namespace Panteon.HistoryStorage.Tests
+{
+    public class MonitorTests : TestBase
+    {
+        const string TASK_NAME = "TEST";
+
+        protected override void FinalizeSetUp()
+        {
+
+        }
+    }
+}
